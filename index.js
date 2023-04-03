70e2976de9f97c511fac714a97901627ec92b71c
Marwahaifawi <maroosh.10102.mm@gmail.com>
2023-04-03 16:45:25 +0300

    change speed slider values & readmeFile

--- index.js
+++ index.js
@@ -332,7 +332,7 @@ function animating(moves) {
   drawArray(array, [move]);
   if (speedSlider.value == 1) speed = 600;
   else if (speedSlider.value == 2) speed = 300;
-  else speed = 100;
+  else speed = 80;
   setTimeout(function () {
     animating(moves);
   }, speed);
